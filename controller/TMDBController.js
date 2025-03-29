e74d2ea134944ca106d9957cfb5c4335f3657025
jon2502 <jonbjarke25@outlook.dk>
2025-03-29 14:40:49 +0100

    started on TMDB integration

--- controller/TMDBController.js
+++ controller/TMDBController.js
@@ -1,7 +1,22 @@
 
+API_BASE_URL = "https://api.themoviedb.org/3"
+
 module.exports = {
-    API_OPTIONS: async function (req, res, next) {
-        
-        next();
+    Popular_Movies: async function (req, res, next) {
+        const URL = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+        const OPTIONS = {
+            method: 'GET',
+             headers: {
+                accept: 'application/json',
+                Authorization: `Bearer fds` 
+            }
+        }
+        fetch(URL, OPTIONS)
+            .then(res => res.json())
+            .then(json => res.json(json))
+            .catch(err => res.json(err));
+    },
+
+    Movie_Search: async function (req, res, next) {
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 TMDBCon = require('../controller/TMDBController.js')
 
 
-router.get('/TMDB',TMDBCon.API_OPTIONS, function(req, res, next) {
-  
+router.get('/TMDB',TMDBCon.Popular_Movies, function(req, res, next) {
+    
 });
 
 module.exports = router;

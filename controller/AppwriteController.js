01f97601aca3d0ceb32af7a17fc8621555fda7e0
jon2502 <jonbjarke25@outlook.dk>
2025-04-01 18:04:40 +0200

    setup Appwrite client

--- controller/AppwriteController.js
+++ controller/AppwriteController.js
@@ -1,4 +1,14 @@
 
+const sdk = require('node-appwrite');
+
+let client = new sdk.Client();
+
+client
+    .setEndpoint(process.env.APPWRITE_ENDPOINT)
+    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY)
+
+
 module.exports = {
     
 }

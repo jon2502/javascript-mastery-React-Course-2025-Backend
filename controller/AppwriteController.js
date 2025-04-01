860fbc9570f1ac872248d024dea44414507715a0
jon2502 <jonbjarke25@outlook.dk>
2025-04-01 18:18:46 +0200

    setup functions for AppWrite

--- controller/AppwriteController.js
+++ controller/AppwriteController.js
@@ -8,6 +8,18 @@ client
     .setProject(process.env.APPWRITE_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY)
 
+const database = new sdk.Databases(client);
+
+const DATABASE_ID = process.env.APPWRITE_DATABASE_ID;
+const COLLECTION_ID = process.env.APPWRITE_COLLECTION_ID;
+
+const updateSearchCount = async (searchTerm, movie) => {
+
+}
+
+const getTrendingMovies = async () =>{
+    
+}
 
 module.exports = {
     

d3a3dfab2dbaad25a0bf46ecc697154bacb3aed8
jon2502 <jonbjarke25@outlook.dk>
2025-04-28 20:39:38 +0200

    Update validators.js

--- middleware/validators.js
+++ middleware/validators.js
@@ -4,7 +4,7 @@ const { body, validationResult } = require('express-validator');
 
 const validateSearchData = [
     body('Title').isString().withMessage('Must be string').notEmpty().withMessage('Username is required'),
-    body('MovieID').isNumeric.withMessage('must be number').notEmpty().withMessage('Username is required'),
+    body('MovieID').isInt.withMessage('must be number').notEmpty().withMessage('Username is required'),
     body('Poster').isURL().withMessage('must be URL').notEmpty().withMessage('Username is required'),
     (req, res, next) => {
         const errors = validationResult(req)
